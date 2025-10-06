8db5587270266c9fde05fb193f97c3583daa1eca
Prashant Rathi <48441582+prashantrathi123@users.noreply.github.com>
2025-10-06 17:10:12 +0530

    update reducer

--- packages/ui/src/commonComponents/SideBar/SideBar.Reducer.js
+++ packages/ui/src/commonComponents/SideBar/SideBar.Reducer.js
@@ -1,15 +1,11 @@
 const initialState = {
     component: "API_TESTER",
-    showFiles: false,
 }
 
 const sideBarReducer = (state = initialState, action) => {
     switch (action.type) {
         case "SET_COMPONENT": return {
             ...state, component: action.component
-        }
-        case "SET_SHOW_FILES": return {
-            ...state, showFiles: action.showFiles
         }
         default: return state
     }
